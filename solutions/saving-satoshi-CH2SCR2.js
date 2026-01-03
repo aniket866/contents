e87068ef622de811fd63ed7059546e43dc79d2a1
Aniket <iamaniketkumarmaner@gmail.com>
2026-01-03 16:41:26 +0530

    Add solution: saving-satoshi-CH2SCR2.js

---
+++ solutions/saving-satoshi-CH2SCR2.js
@@ -0,0 +1,9 @@
+const crypto = require('crypto')
+
+// Create a program that finds a sha256 hash starting with 5 zeroes.
+// To submit your answer, return it from the function.
+
+function findHashFromNonce(nonce) {
+  // Type your code here
+}
+  
