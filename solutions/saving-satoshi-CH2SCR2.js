dfc1bfe81ba8a0e7fe400a09a8607f3eac24965d
Aniket <iamaniketkumarmaner@gmail.com>
2026-01-03 18:52:00 +0530

    Delete solutions directory

--- solutions/saving-satoshi-CH2SCR2.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto')
-
-// Create a program that finds a sha256 hash starting with 5 zeroes.
-// To submit your answer, return it from the function.
-
-function findHashFromNonce(nonce) {
-  // Type your code here
-}
-  
